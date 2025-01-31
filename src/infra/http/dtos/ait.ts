b12d4e569276fde7ccd45cac208739debf7dc86b
Melk de Sousa <melk.sousap@gmail.com>
2025-01-30 23:05:04 -0300

    refactor: rename and restructure AIT DTOs for clarity and consistency

--- src/infra/http/dtos/ait.ts
+++ src/infra/http/dtos/ait.ts
@@ -2,7 +2,30 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Decimal } from '@prisma/client/runtime/library';
 import { Transform } from 'class-transformer';
 
-export class UpdateAITDto {
+export class CreateAITInputDto {
+  @ApiProperty({ example: 'ABC1234' })
+  placa_veiculo: string;
+
+  @ApiProperty({ example: '2025-01-28T12:00:00Z' })
+  data_infracao: Date;
+
+  @ApiProperty({ example: 'Estacionamento proibido' })
+  descricao: string;
+
+  @ApiProperty({ example: 150.75 })
+  valor_multa: number;
+}
+
+export class ProcessAitsInputDTO {
+  @ApiProperty({ example: '2025-01-28T12:00:00Z' })
+  startDate: Date;
+
+  @ApiProperty({ example: '2025-01-28T12:00:00Z' })
+  endDate: Date;
+}
+
+
+export class UpdateAITInputDto {
   @ApiProperty({ example: 'ABC1234' })
   placa_veiculo: string;
 
