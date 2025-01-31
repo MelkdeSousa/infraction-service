659e3a1b4afee7d6fc8d9edac8218238b744f420
Melk de Sousa <melk.sousap@gmail.com>
2025-01-30 23:18:55 -0300

    refactor: simplify AIT entity by integrating validation into schema and updating constructor

--- src/domain/ait.entity.ts
+++ src/domain/ait.entity.ts
@@ -1,135 +1,59 @@
-import { z } from 'zod';
-
 import { Decimal } from '@prisma/client/runtime/library';
 import { handleSafeParseZod } from 'src/lib/handleSafeParseZod';
+import { z } from 'zod';
 
-export interface AITProps {
-  placaVeiculo: string;
-  dataInfracao: Date;
-  descricao: string;
-  valorMulta: Decimal;
-}
+export const aitSchema = z.object({
+  id: z.string().optional(),
+  placaVeiculo: z.string().min(7, 'a placa do veículo precisa ter no mínimo 7 caracteres'),
+  dataInfracao: z.date(),
+  descricao: z.string()
+    .min(10, 'descrição precisa ter no mínimo 10 caracteres')
+    .max(100, 'descrição precisa ter no máximo 100 caracteres'),
+  valorMulta: z.instanceof(Decimal)
+    .refine((value) => value.gt(new Decimal(0)), {
+      message: 'o valor da multa precisa ser maior que zero',
+    })
+});
+
+export type AITProps = z.infer<typeof aitSchema>;
 
 export class AIT {
-  private _id: string;
-  private _placaVeiculo: string;
-  private _dataInfracao: Date;
-  private _descricao: string;
-  private _valorMulta: Decimal;
+  public id?: string;
+  public placaVeiculo: string;
+  public dataInfracao: Date;
+  public descricao: string;
+  public valorMulta: Decimal;
 
-  constructor(props: AITProps) {
-    this.validate(props);
-    this._placaVeiculo = props.placaVeiculo;
-    this._dataInfracao = props.dataInfracao;
-    this._descricao = props.descricao;
-    this._valorMulta = props.valorMulta;
+  private constructor(props: AITProps) {
+    this.id = props.id;
+    this.placaVeiculo = props.placaVeiculo;
+    this.dataInfracao = props.dataInfracao;
+    this.descricao = props.descricao;
+    this.valorMulta = props.valorMulta;
   }
 
-  private validate(props: AITProps): void {
-    const schema = z.object({
-      placaVeiculo: z
-        .string()
-        .min(7, 'a placa do veículo precisa ter no mínimo 7 caracteres'),
-      dataInfracao: z.date(),
-      descricao: z
-        .string()
-        .min(10, 'descrição precisa ter no mínimo 10 caracteres')
-        .max(100, 'descrição precisa ter no máximo 100 caracteres'),
-      valorMulta: z
-        .instanceof(Decimal)
-        .refine((value) => value.gt(new Decimal(0)), {
-          message: 'o valor da multa precisa ser maior que zero',
-        }),
-    });
-
-    const result = schema.safeParse(props);
-
-    if (result.success === false) throw handleSafeParseZod(result);
-  }
-  public updateAIT(
-    placaVeiculo: string,
-    dataInfracao: Date,
-    descricao: string,
-    valorMulta: Decimal,
-  ) {
-    // Criando objeto com novos valores
-    const updatedProps: AITProps = {
-      placaVeiculo,
-      dataInfracao,
-      descricao,
-      valorMulta,
+  public update(props: Partial<Omit<AITProps, 'id'>>) {
+    const updatedAit = {
+      placaVeiculo: props.placaVeiculo ?? this.placaVeiculo,
+      dataInfracao: props.dataInfracao ?? this.dataInfracao,
+      descricao: props.descricao ?? this.descricao,
+      valorMulta: props.valorMulta ?? this.valorMulta,
     };
 
-    // Revalidando os novos valores
-    this.validate(updatedProps);
+    const result = aitSchema.omit({ id: true }).safeParse(updatedAit);
+    if (result.success === false) throw handleSafeParseZod(result);
 
-    // Atualizando os valores apenas se forem válidos
-    this._placaVeiculo = placaVeiculo;
-    this._dataInfracao = dataInfracao;
-    this._descricao = descricao;
-    this._valorMulta = valorMulta;
-  }
-  public get id() {
-    return this._id;
-  }
-
-  // Getters e Setters para os campos restantes
-
-  public get placaVeiculo() {
-    return this._placaVeiculo;
-  }
-
-  public set placaVeiculo(value: string) {
-    if (value.length < 7) {
-      throw new Error('A placa do veículo precisa ter no mínimo 7 caracteres');
-    }
-    this._placaVeiculo = value;
-  }
-
-  public get dataInfracao() {
-    return this._dataInfracao;
-  }
-
-  public set dataInfracao(value: Date) {
-    if (isNaN(value.getTime())) {
-      throw new Error('Data de infração inválida');
-    }
-    this._dataInfracao = value;
-  }
-
-  public get descricao() {
-    return this._descricao;
-  }
-
-  public set descricao(value: string) {
-    if (value.length < 10 || value.length > 100) {
-      throw new Error(
-        'Descrição precisa ter no mínimo 10 caracteres e no máximo 100 caracteres',
-      );
-    }
-    this._descricao = value;
-  }
-
-  public get valorMulta() {
-    return this._valorMulta;
-  }
-
-  public set valorMulta(value: Decimal) {
-    if (value.toNumber() <= 0) {
-      throw new Error('O valor da multa precisa ser maior que zero');
-    }
-    this._valorMulta = value;
+    Object.assign(this, updatedAit);
   }
 
   static create(props: AITProps): [AIT | null, Error | null] {
     try {
+      const result = aitSchema.safeParse(props);
+      if (result.success === false) throw handleSafeParseZod(result);
+
       return [new AIT(props), null];
     } catch (error) {
       return [null, error as Error];
     }
-  }
-
-  static instance(props: AITProps): [AIT | null, Error | null] {
-    return [new AIT(props), null];
   }
 }
